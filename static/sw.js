13f3308eff213fbd3b1e0b9a05adc9990952cdb4
planethouki <26595148+planethouki@users.noreply.github.com>
2019-03-13 20:43:45 +0900

    commit

--- static/sw.js
+++ static/sw.js
@@ -2,31 +2,31 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/1edf615260a3f068a402.js",
-    "revision": "8801b2a0d421e123d5988c895a4e6c7c"
+    "url": "/_nuxt/0a86b68ba6c142703c66.js",
+    "revision": "4a77dc11a491e447bf7787481639051f"
   },
   {
-    "url": "/_nuxt/5567222343dfccbb88ee.js",
-    "revision": "73cbdf4c1064501d62424ba8397e219f"
+    "url": "/_nuxt/0dfc6146582be668807f.js",
+    "revision": "faab2a92f343877d23852d65d74dd55e"
   },
   {
-    "url": "/_nuxt/69805e477e38bd43fbe5.js",
-    "revision": "b2b4a89dcfb89aa0cc07cd99c41263b3"
+    "url": "/_nuxt/0f9aa226c51815687558.js",
+    "revision": "95157a36613a031e87d4e1058f5753df"
   },
   {
-    "url": "/_nuxt/973c9b393ff0f0c013cb.js",
-    "revision": "fd3d890deb9d97cda2dcb14fb0288f02"
+    "url": "/_nuxt/2d6f480b89ea437030d8.js",
+    "revision": "3ee8be4e1a996eb4d370e78ad7a77ea1"
   },
   {
-    "url": "/_nuxt/d8a97d1c5f111b0396b5.js",
-    "revision": "2865e3d77f715df5f26d6bb73eed27fa"
+    "url": "/_nuxt/38325e334ac21e7ef6c9.js",
+    "revision": "a87512fcab4706976edfa784efdc6236"
   },
   {
-    "url": "/_nuxt/f12b949adada0ddf5ed9.js",
-    "revision": "efbf5fbd058f68b95f9a12a104742b38"
+    "url": "/_nuxt/db57a51c4f838411602b.js",
+    "revision": "7aa4cf7ade0dc4715e13e68f811dc185"
   }
 ], {
-  "cacheId": "nem2-unsafe-wallet-nuxt",
+  "cacheId": "nem2-test-wallet-nuxt",
   "directoryIndex": "/",
   "cleanUrls": false
 })
