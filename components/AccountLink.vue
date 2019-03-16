30ff397e6b47d125cabcdb1f11741c3bb0f816f8
planethouki <26595148+planethouki@users.noreply.github.com>
2019-03-16 18:40:29 +0900

    account link part

--- components/AccountLink.vue
+++ components/AccountLink.vue
@@ -2,23 +2,33 @@
   v-flex(mb-5 v-if="wallet.address" v-bind:id="navTargetId")
     v-card
       v-card-title
-        div.title Account Link (Not yet)
+        div.title Account Link (Not yet
       v-card-text
-        v-radio-group(label="Link Type" row)
-          v-radio(label="Link")
-          v-radio(label="Unlink")
+        v-radio-group(label="Link Action" v-model="linkAction" row)
+          v-radio(
+          v-for="pt in linkActions"
+          :key="pt.type"
+          :label="pt.label"
+          :value="pt.type")
         v-text-field(
           label="Remote Account Public Key"
-          v-model="remoteAccountKey")
+          v-model="remoteAccountKey"
+          :counter="64"
+          required)
         v-text-field(
           label="Max Fee"
           v-model="fee")
+      v-card-actions
+        v-btn(
+        color="blue"
+        class="white--text"
+        @click="announceHandler") announce
       v-card-text
         tx-history(v-bind:history="history")
 </template>
 
 <script>
-import { Account } from 'nem2-sdk'
+import { Account, LinkAction, AccountLinkTransaction, Deadline, UInt64, TransactionHttp } from 'nem2-sdk'
 import TxHistory from './TxHistory.vue'
 
 export default {
@@ -35,14 +45,45 @@ export default {
   data() {
     return {
       remoteAccountKey: '',
+      linkAction: LinkAction.Link,
+      linkActions: [
+        { type: LinkAction.Link, label: 'Link' },
+        { type: LinkAction.Unlink, label: 'Unlink' }
+      ],
       fee: 0,
       history: []
     }
   },
-  mounted: function () {
-    this.remoteAccountKey = Account.generateNewAccount(this.wallet.network).publicKey
+  watch: {
+    wallet: {
+      handler: function () {
+        if (this.wallet.address) {
+          this.remoteAccountKey = Account.generateNewAccount(this.wallet.network).publicKey
+        }
+      }
+    }
   },
   methods: {
+    announceHandler: function (event) {
+      const account = this.wallet.open(this.walletPassword)
+      const endpoint = this.endpoint
+      const accountLinkTransaction = new AccountLinkTransaction(
+        this.wallet.network,
+        this.$TransactionVersion.LINK_ACCOUNT,
+        Deadline.create(),
+        UInt64.fromUint(this.fee),
+        this.remoteAccountKey,
+        this.linkAction
+      )
+      const signedTx = account.sign(accountLinkTransaction)
+      const txHttp = new TransactionHttp(endpoint)
+      txHttp.announce(signedTx)
+      const historyData = {
+        hash: signedTx.hash,
+        apiStatusUrl: `${endpoint}/transaction/${signedTx.hash}/status`
+      }
+      this.history.push(historyData)
+    }
   }
 }
 </script>
