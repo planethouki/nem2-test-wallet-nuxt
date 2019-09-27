9daf2f6d16f0f8f639d867741787d9864be200ec
planethouki <26595148+planethouki@users.noreply.github.com>
2019-09-27 12:59:12 +0900

    env

--- store/env.js
+++ store/env.js
@@ -3,11 +3,11 @@ import { NetworkType } from 'nem2-sdk'
 let endpointList, defaultEndpoint, mosaicPlaceholder
 if (process.env.isNf) {
   endpointList = [
-    { url: 'http://3.1.202.148:3000', label: 'my-8' },
+    { url: 'http://103.3.60.174:3000', label: 'my-8' },
     { url: 'http://13.114.200.132:3000', label: 'jp-5' },
-    { url: 'http://47.107.245.217:3000', label: 'cn-2' }
+    { url: 'http://47.108.88.254:3000', label: 'cn-3' }
   ]
-  defaultEndpoint = 'http://3.1.202.148:3000'
+  defaultEndpoint = 'http://103.3.60.174:3000'
   mosaicPlaceholder = {
     transfer: '@nem.xem::0',
     currency10: '@nem.xem::10000000',
@@ -16,14 +16,14 @@ if (process.env.isNf) {
   }
 } else {
   endpointList = [
-    { url: 'http://elephant2.48gh23s.xyz:3000', label: 'elephant2.48gh23s.xyz' }
+    { url: 'http://elephant3.48gh23s.xyz:3000', label: 'elephant3.48gh23s.xyz' }
   ]
-  defaultEndpoint = 'http://elephant2.48gh23s.xyz:3000'
+  defaultEndpoint = 'http://elephant3.48gh23s.xyz:3000'
   mosaicPlaceholder = {
-    transfer: '@cat.currency::0, 941299B2B7E1291C::0',
+    transfer: '@cat.currency::0, 1292A7F2F8AA2DE6::0',
     currency10: '@cat.currency::10000000',
-    escrow1: '@cat.currency::0, 941299B2B7E1291C::0',
-    escrow2: '85BBEA6CC462B244::0, @cat.harvest::0'
+    escrow1: '@cat.currency::0, 1292A7F2F8AA2DE6::0',
+    escrow2: '35DA50B9E4FD465F::0, @cat.harvest::0'
   }
 }
 
